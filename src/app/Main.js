f30393f6b9032ea79ce992c84daac321a778395c
BIT Student <classroom@bgit.rs>
2019-04-08 12:16:30 +0200

    main fix

--- src/app/Main.js
+++ src/app/Main.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react"
+import { Switch, Route } from "react-router-dom"
+import Profile from "../components/Profile/Profile";
+import People from "../components/People/People";
+import Feed from "../components/Feed/Feed";
 
 const Main = () => {
 
     return (
-        <main>
-
+        <main className='container'>
+            <Switch>
+                <Route path="/profile" component={Profile} />
+                <Route path="/people" component={People} />
+                <Route path="/" component={Feed} />
+            </Switch>
         </main>
 
     )
