fb026d7a6b06baaa12105f392ac33dc75c13e970
BIT Student <classroom@bgit.rs>
2019-04-10 14:51:00 +0200

    Commenting

--- src/components/Posts/PostList.js
+++ src/components/Posts/PostList.js
@@ -3,11 +3,14 @@ import Post from '../../entities/Post'
 
 const PostList = ({ posts }) => {
 
+    let tempArray = posts;
+    let reversed = tempArray.reverse();
+
     return (
         <div className='row'>
             <div className='col-2'>   </div>
             <div className='col-8'>
-                {posts.map(post => <Post key={post.id} id={post.id} type={post.type} content={post.type === 'text' ? post.text : post.type === 'image' ? post.imageUrl : post.videoUrl} />)}
+                {reversed.map(post => <Post key={post.id} id={post.id} type={post.type} content={post.type === 'text' ? post.text : post.type === 'image' ? post.imageUrl : post.videoUrl} />)}
             </div>
         </div>
     )
